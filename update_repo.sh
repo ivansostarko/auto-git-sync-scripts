79f4730436815220173cf111c50d6ab5a9882e79
admin <mrkva.bond@proton.me>
2025-10-18 13:46:57 +0200

    Automated backup: 2025-10-18 13:46:57

--- update_repo.sh
+++ update_repo.sh
@@ -16,3 +16,23 @@ git pull origin "${BRANCH}"
 
 # Log success
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Repository fast-synced to origin/${BRANCH}." >> "$LOG_FILE"
+
+
+
+
+#!/bin/bash
+set -euo pipefail
+
+# --- Settings ---
+REPO_DIR="/mnt/github-ivan/windays-php-app"
+BRANCH="main"
+LOG_FILE="/mnt/github-ivan/windays-php-app/update_repo.log"
+
+# Go to repo
+cd "$REPO_DIR" || exit 1
+
+# Pull latest commits
+git pull 
+
+# Log success
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup." >> "$LOG_FILE"
